048403d852103c74268a350b4df2162d1bc3e1f4
lct45 <lthomas@confluent.io>
2020-09-02 15:07:47 -0500

    temp update for topology test

--- streams/examples/src/main/java/org/apache/kafka/streams/examples/temperature/TemperatureDemo.java
+++ streams/examples/src/main/java/org/apache/kafka/streams/examples/temperature/TemperatureDemo.java
@@ -97,63 +97,12 @@ public class TemperatureDemo {
 
     public static void main(final String[] args) {
 
-        final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-temperature");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-
         final Topology topology = getTopology();
 
         final String currentTopologyDescription = topology.describe().toString();
         System.out.println(currentTopologyDescription);
 
-        final StreamsBuilder builder = new StreamsBuilder();
 
-        final KStream<String, String> source = builder.stream("iot-temperature");
-
-        final KStream<Windowed<String>, String> max = source
-            // temperature values are sent without a key (null), so in order
-            // to group and reduce them, a key is needed ("temp" has been chosen)
-            .selectKey((key, value) -> "temp")
-            .groupByKey()
-            .windowedBy(TimeWindows.of(Duration.ofSeconds(TEMPERATURE_WINDOW_SIZE)))
-            .reduce((value1, value2) -> {
-                if (Integer.parseInt(value1) > Integer.parseInt(value2)) {
-                    return value1;
-                } else {
-                    return value2;
-                }
-            })
-            .toStream()
-            .filter((key, value) -> Integer.parseInt(value) > TEMPERATURE_THRESHOLD);
-
-        final Serde<Windowed<String>> windowedSerde = WindowedSerdes.timeWindowedSerdeFrom(String.class);
-
-        // need to override key serde to Windowed<String> type
-        max.to("iot-temperature-max", Produced.with(windowedSerde, Serdes.String()));
-
-        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        // attach shutdown handler to catch control-c
-        Runtime.getRuntime().addShutdownHook(new Thread("streams-temperature-shutdown-hook") {
-            @Override
-            public void run() {
-                streams.close();
-                latch.countDown();
-            }
-        });
-
-        try {
-            streams.start();
-            latch.await();
-        } catch (final Throwable e) {
-            System.exit(1);
-        }
         System.exit(0);
     }
 
